13d4bc56a9b7a1f5ac65f023d332f064083ad20c
Qzjl14629 <jliu294@buffalo.edu>
2026-02-16 12:23:26 -0500

    updated QRCodePage to include the pathway for the Mobile Page

--- frontend/src/components/QRCodePage.tsx
+++ frontend/src/components/QRCodePage.tsx
@@ -1,11 +1,11 @@
 import QRCode from "react-qr-code";
 export default function generateQRCode({room}: { room: string }){
-    const baseURL = "https://www.google.com/"
-    const link = baseURL + `?q=${room}`;
+    const baseURL = ""//include the url of the home page here
+    const link = baseURL + `/mobile?q=${encodeURIComponent(room)}`;
     return (
         <QRCode
         value = {link} 
-        size={100}
+        size={150}
 
         />
     )
