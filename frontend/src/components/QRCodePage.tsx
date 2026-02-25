ce1a8a0849b9c22733535aea8886ea190d5d3e8c
Puru Soni <purusoni@buffalo.edu>
2026-02-25 12:54:03 -0500

    fix qrcode base url

--- frontend/src/components/QRCodePage.tsx
+++ frontend/src/components/QRCodePage.tsx
@@ -1,11 +1,11 @@
 import QRCode from "react-qr-code";
-export default function generateQRCode({room}: { room: string }){
-    const baseURL = ""//include the url of the home page here
+export default function generateQRCode({ room }: { room: string }) {
+    const baseURL = window.location.origin;
     const link = baseURL + `/mobile?q=${encodeURIComponent(room)}`;
     return (
         <QRCode
-        value = {link} 
-        size={150}
+            value={link}
+            size={150}
 
         />
     )
